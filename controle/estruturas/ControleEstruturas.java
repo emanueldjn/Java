f95a436edb70fab2979dbcd439a2e63210d9477f
Emanuel <emanuelnas29@gmail.com>
2025-12-05 10:04:23 -0300

    controle de estruturas, if/else pt1

---
+++ controle/estruturas/ControleEstruturas.java
@@ -0,0 +1,12 @@
+package controle.estruturas;
+
+public class ControleEstruturas {
+    public static void main(String[] args) {
+        int idade = 15;
+        if(idade >= 18) {
+            System.out.println("pode dirigir!");
+        } else {
+            System.out.println("Não pode Dirigir!");
+        }
+    }
+}
